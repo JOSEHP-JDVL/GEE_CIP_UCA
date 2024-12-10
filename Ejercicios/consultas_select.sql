8a37a991852487f318b2422c780dd891bb8f7f51
JOSEHP <josehp.jdvl@gmail.com>
2024-12-09 22:58:43 -0500

    09-12-2024_2

--- Ejercicios/consultas_select.sql
+++ Ejercicios/consultas_select.sql
@@ -13,3 +13,36 @@ SELECT
 FROM personas_naturales
 WHERE nombres='Camila';
 
+-- ENCONTRAR FILAS QUE CONTIENEN UN VALOR COMO PARTE DE UNA CADENA.
+-- SELECCIONAR TODOS LAS PERSONAS NATURALES CUYO APELLIDO PATERNO CONTENGAN LA CADENA DE "ez"
+
+
+SELECT 
+
+	numero_documento AS 'DNI',
+	CONCAT(nombres,' ',apellido_paterno,' ',apellido_materno) AS 'Nombre completo',
+	email
+
+FROM personas_naturales
+WHERE apellido_paterno LIKE ('%ez%');
+
+-- LOS APELLIDOS PATERNOS QUE EMPIECEN CON "R"
+SELECT 
+
+	numero_documento AS 'DNI',
+	CONCAT(nombres,' ',apellido_paterno,' ',apellido_materno) AS 'Nombre completo',
+	email
+
+FROM personas_naturales
+WHERE apellido_paterno LIKE ('R%');
+
+-- LOS APELLIDOS PATERNOS QUE TERMINEN CON "S"
+
+SELECT 
+
+	numero_documento AS 'DNI',
+	CONCAT(nombres,' ',apellido_paterno,' ',apellido_materno) AS 'Nombre completo',
+	email
+
+FROM personas_naturales
+WHERE apellido_paterno LIKE ('%S');
